6a5c4dfd20ad1730ec30ff62b2658476fd7561da
quockhanhlsks123 <quockhanhlsls44@gmail.com>
2023-07-27 01:55:04 +0700

    WithRouter

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,14 +1,17 @@
 import react from 'react';
 import './Nav.scss';
+import {
+    Link, NavLink
+} from "react-router-dom";
 
 class Nav extends react.Component {
     render() {
         return (
             <>
                 <div className="topnav">
-                    <a className="active" href="/">Home</a>
-                    <a href="/todo">Todos</a>
-                    <a href="/about">About</a>
+                    <NavLink to="/" activeClassName="active" exact>Home</NavLink>
+                    <NavLink to="/todo" activeClassName="active" >Todo</NavLink>
+                    <NavLink to="/about" activeClassName="active" >About</NavLink>
                 </div>
             </>
         )
